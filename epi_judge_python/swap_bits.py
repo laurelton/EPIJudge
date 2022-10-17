3164e5f0c57ea4c518dbcc838f6e842e06a5d3fa
Laurelton <name@example.com>
2022-10-17 10:53:12 -0400

    4.02 - swap bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    # Shift x right by 'i' bits then 'and' 1 to test if the bit is set
+    i_is_set = (x >> i) & 1 
+    j_is_set = (x >> j) & 1
+    # If the bits are the same we don't need to swap them
+    if i_is_set != j_is_set:
+        # Create a bit mask where only 'i' and 'j' are set
+        # (1 << i) will set the ith bit
+        # 'or' creates a mask with both bits set
+        bit_mask = (1 << i) | (1 << j)
+        # 'xor' flips bits
+        x = x ^ bit_mask
+
+    return x
 
 
 if __name__ == '__main__':

5b5955b2be32bc4f35bea4741784578b8a7266fc
Laurelton <name@example.com>
2022-10-17 11:57:39 -0400

    4.09 - is number palindromic

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,29 @@
 from test_framework import generic_test
-
+from math import floor, log10
 
 def is_palindrome_number(x: int) -> bool:
-    # TODO - you fill in here.
+    if x < 0:
+        return False
+
+    if x < 10: 
+        return True
+
+    num_digits = floor(log10(x)) + 1
+    msd_mask = 10 ** (num_digits - 1)
+
+    for _ in range(num_digits // 2):
+        first = x // msd_mask
+        last = x % 10
+        if first != last:
+            return False
+
+        # Remove first digit
+        x = x % msd_mask
+        # Remove last digit
+        x = x // 10
+        # Adjust Most Significant Digit mask
+        msd_mask = msd_mask // 100
+
     return True
 
 

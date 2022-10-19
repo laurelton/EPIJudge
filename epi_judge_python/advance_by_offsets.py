27f1fa038efc2350bd63c2ab1b041cde915af77e
Laurelton <name@example.com>
2022-10-18 20:12:17 -0400

    5.04 - advance by offset - forward

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,8 +4,12 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    max_index = 0
+    for i, val in enumerate(A):
+        if max_index >= i:
+            max_index = max(val + i, max_index)
+
+    return max_index >= len(A) - 1
 
 
 if __name__ == '__main__':

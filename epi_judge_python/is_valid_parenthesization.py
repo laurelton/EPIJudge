cb020179415bd459db05aca5430783aa08373f98
Laurelton <name@example.com>
2022-11-11 11:08:23 -0500

    8.03 - well formed string

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,8 +2,23 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    prev_chars = []
+    close_dict = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+
+    for char in s:
+        if char not in close_dict:
+            prev_chars.append(char)
+        else:
+            if len(prev_chars) and close_dict[char] == prev_chars[-1]:
+                prev_chars.pop()
+            else:
+                return False
+
+    return len(prev_chars) == 0
 
 
 if __name__ == '__main__':

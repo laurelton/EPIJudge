1632cbcbcc440c99efb9428164b0a0609611d7d9
Laurelton <name@example.com>
2022-11-13 11:14:03 -0500

    8.04 - normalize pathnames

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    prepend_slash = path.startswith('/')
+    dirs = path.split('/')
+    output = []
+
+    for dir in dirs:
+        if dir == '.' or dir == '':
+            continue
+        elif dir == '..' and len(output) and output[-1] != '..':
+            output.pop()
+        else:
+            output.append(dir)
+
+    if prepend_slash and len(output):
+        output[0] = f'/{output[0]}'
+
+    if not len(output):
+        output.append('/')
+
+    return '/'.join(output)
 
 
 if __name__ == '__main__':

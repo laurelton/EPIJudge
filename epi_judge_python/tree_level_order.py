6cdbd9bda87bf106b6664dbd72c9e5cfb701aeba
Laurelton <name@example.com>
2022-11-13 12:03:45 -0500

    8.06 - tree level order

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -1,12 +1,32 @@
 from typing import List
+from collections import deque
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    if not tree:
+        return result
+
+    curr_level = [tree]
+    while curr_level:
+        level_data = []
+        for node in curr_level:
+            level_data.append(node.data)
+
+        result.append(level_data)
+        child_nodes = []
+        for node in curr_level:
+            if node.left:
+                child_nodes.append(node.left)
+            if node.right:
+                child_nodes.append(node.right)
+
+        curr_level = child_nodes
+
+    return result
 
 
 if __name__ == '__main__':
